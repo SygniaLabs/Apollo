57c59eecbbbc9383cc6e9d30934c701e8a88394b
Ol3gl3 <oleg.l@sygnia.co>
2023-06-02 12:58:41 +0800

    integrate C3 profile

--- Payload_Type/apollo/apollo/agent_code/Apollo/Config.cs
+++ Payload_Type/apollo/apollo/agent_code/Apollo/Config.cs
@@ -2,6 +2,7 @@
 
 #if DEBUG
 #define HTTP
+//#define C3
 #endif
 
 #if HTTP
@@ -16,6 +17,9 @@ using PSKCryptography;
 using ApolloInterop.Serializers;
 #if SMB
 using NamedPipeTransport;
+#endif
+#if C3
+using C3Transport;
 #endif
 #if TCP
 using TcpTransport;
@@ -84,7 +88,31 @@ namespace Apollo
                     }
                 }
             },
-#elif TCP
+#endif
+#if C3
+            { "c3", new C2ProfileData()
+                {
+                    TC2Profile = typeof(C3Profile),
+                    TCryptography = typeof(PSKCryptographyProvider),
+                    TSerializer = typeof(EncryptedJsonSerializer),
+                    Parameters = new Dictionary<string, string>()
+                    {
+#if DEBUG
+                        { "pipename", "ahatojqq-bo0w-oc3r-wqtg-4jf7voepqqbs" },
+                        { "callback_interval", "40" },
+                        { "callback_jitter", "0" },
+                        { "encrypted_exchange_check", "T" },
+#else
+                        { "pipename", "pipename_here" },
+                        { "callback_interval", "callback_interval_here" },
+                        { "callback_jitter", "callback_jitter_here" },
+                        { "encrypted_exchange_check", "encrypted_exchange_check_here" },
+#endif
+                    }
+                }
+            },
+#endif
+#if TCP
             { "tcp", new C2ProfileData()
                 {
                     TC2Profile = typeof(TcpProfile),
@@ -103,22 +131,26 @@ namespace Apollo
                 }
             }
 #endif
-                    };
+        };
 
 
         public static Dictionary<string, C2ProfileData> IngressProfiles = new Dictionary<string, C2ProfileData>();
 #if DEBUG
 #if HTTP
-        public static string StagingRSAPrivateKey = "ejezYqXCDzFUWbgyJ7BMVqCKfi02jOGUx4ZtAc1rllM=";
+        public static string StagingRSAPrivateKey = "yKspD9WUraWq3tUl2MKRaF0cHGf85K0gFnZj0olZzaQ=";
 #elif SMB
         public static string StagingRSAPrivateKey = "cnaJ2eDg1LVrR5LK/u6PkXuBjZxCnksWjy0vEFWsHIU=";
+#elif C3
+        public static string StagingRSAPrivateKey = "pvamqNAN2v56eh/SibXQxOZWJCzG0JorUqBpd4+sdlI=";
 #elif TCP
         public static string StagingRSAPrivateKey = "LbFpMoimB+aLx1pq0IqXJ1MQ4KIiGdp0LWju5jUhZRg=";
 #endif
 #if HTTP
-        public static string PayloadUUID = "084ece5f-7130-4a5a-81af-55e332802751";
+        public static string PayloadUUID = "74579531-d8d5-4f7b-9074-eb39088abe4d";
 #elif SMB
         public static string PayloadUUID = "869c4909-30eb-4a90-99b2-874dae07a0a8";
+#elif C3
+        public static string PayloadUUID = "764a3a8d-21a1-4380-a9b0-4a6dbb266826";
 #elif TCP
         public static string PayloadUUID = "a51253f6-7885-4fea-9109-154ecc54060d";
 #endif
